81f0a1f492286a49784272df9aa69393d5ee5656
mtfp10 <ferreira.pinheiro.matheus@escola.pr.gov.br>
2025-11-10 09:59:11 -0300

    commit

--- js/principal.js
+++ js/principal.js
@@ -1,19 +1,38 @@
 var titulo = document.querySelector(".titulo");
-			titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Aparecida Nutricionista";
 
-/*  IMC = massa/altura^2      */
+/*  IMC = massa/altura^2    */
 var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = document.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-
-var tdAltura = document.query.Selector(".info-altura");
+var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
-var imc = peso / (altura*altura);
-
-console.log(paciente); //tr
-console.log(tdPeso); // td que tem peso
-console.log(tdAltura); //td que tem altura
-console.log(peso); //valor do peso
-console.log(altura); //valor da altura
-
 var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+//validar os dados de peso
+if (peso <= 0 ||  peso > 1000) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+//validar a altura
+if (altura <=0 || altura > 3){
+    console.log("Altura inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+//validar peso e altura
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura*altura);
+    tdImc.textContent = imc;
+}
+
+/*
+console.log(paciente);  //tr
+console.log(tdPeso);    // td que tem peso
+console.log(tdAltura);  //td que tem altura
+console.log(peso);      //valor do peso
+console.log(altura);    //valor da altura
+*/
